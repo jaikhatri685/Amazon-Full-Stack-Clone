9498a2a789d87c48449925c5548dc83bba52453b
Jai Khatri <f20190543@goa.bits-pilani.ac.in>
2022-08-08 18:08:05 +0530

    fix

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ export default function Home(){
 
  
     return <div className="pl-10 pr-10">
-        <img src={poster_home} className="lg:-mb-52 sm:-mb-32 md:-mb-32"/>
+        <img src={poster_home} className="-mb-52 -mb-10 md:-mb-25"/>
         <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-5 pl-5 pr-5 ">
             {productData.data.map((item,index)=>{return(<Product id= {uuid()} title={item.title} image={item.image } price={item.price} rating={item.rating}/>)})}
         </div>
